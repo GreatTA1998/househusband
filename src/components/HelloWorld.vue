d823f9a83dfb226c80c3030f361ea9544b7c40e8
EltonLin <>
2021-05-21 21:30:01 +0000

    Work in progress

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,6 @@
 
   <p>
     <div>
-      Finance
-    </div>
-    <div>
       Sleep
     </div>
     <div>
@@ -18,15 +15,8 @@
       Important people
     </div>
 
-
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
 
@@ -39,9 +29,3 @@ defineProps({
 
 const state = reactive({ count: 0 })
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/InspiringThingsToFollow.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Jeremy Lin
+    Spotify
+    Tia, Yorushika, Joe Inoue
+    Schubert 
+
+    Twitter
+    Y-Combinator 
+    Paul Graham 
+
+    MLB, NBA
+    Scherzer, Steph Curry
+  </div>
+</template>
